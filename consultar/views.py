bc24bf56ff6036e24a3ad7038be0191f1da2b74c
arthuurgab <adarocha060@gmail.com>
2025-11-17 10:03:46 -0300

    feat: criar views dos Detalhes

--- consultar/views.py
+++ consultar/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Detalhes
 
-# Create your views here.
+def DetalheView(request):
+    dados = None
+    erro = None
+
+    if request.method == 'POST':
+        cnpj = request.POST.get("cnpj")
+
+        try: 
+            dados = Detalhes.objects.get(cnpj=cnpj)
+        except Detalhes.DoesNotExist:
+            erro = "CNPJ não encontrado."
+    
+    return render(request, "detalhes.html", {"dados": dados, "erro": erro})
