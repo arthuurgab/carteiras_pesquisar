09501d8379b01b4c54ef0b6d600e82f30ac0526f
arthuurgab <adarocha060@gmail.com>
2025-11-18 17:04:11 -0300

    feat: logout adicionado

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('contas/', include('contas.urls')),
     
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
